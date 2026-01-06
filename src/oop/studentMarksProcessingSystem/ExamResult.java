5227caf166df0ec5fe0b394f6ea0a4471b08c109
ACRATHNAYAKA <avishkachamara71@gmail.com>
2026-01-06 22:14:31 +0530

    feat:complete student Mark Processing System

--- src/oop/studentMarksProcessingSystem/ExamResult.java
+++ src/oop/studentMarksProcessingSystem/ExamResult.java
@@ -2,35 +2,76 @@ package oop.studentMarksProcessingSystem;
 
 public class ExamResult {
     private final Student student;
-    private double[] marks = new double[5];
-    private String [] subject = new String[5];
+    private final double[] marks;
+    private final String [] subject;
 
     private double totalMarks;
     public ExamResult(Student student, double [] marks, String [] subject){
         if (marks == null || marks.length == 0){
             throw new IllegalArgumentException("Marks Array Cant be Null or Empty");
         }
+
+        for (double m: marks){
+            if (m>100 || m<0){
+                throw new IllegalArgumentException("Marks rang is 0 - 100");
+            }
+        }
+
         if (subject == null || subject.length == 0){
             throw  new IllegalArgumentException("Subject Array cant be Null or Empty");
         }
 
+        if (student==null){
+            throw new IllegalArgumentException("Student Object cant be null");
+        }
+
+        if (marks.length != subject.length){
+            throw new IllegalArgumentException("Array length not Equal");
+        }
+
         this.student =student;
-        this.marks = marks;
-        this.subject = subject;
+        this.marks = marks.clone();
+        this.subject = subject.clone();
+
+        calculateTotalMarks();
     }
 
     private void calculateTotalMarks(){
-        for (double marks: marks){
-            totalMarks+=marks;
+        for (double m: marks){
+            totalMarks+=m;
         }
     }
 
     public double getAverage(){
-        return totalMarks/5;
+        return totalMarks/subject.length;
     }
 
-    public double getTotal(){
+    public double getTotal() {
         return totalMarks;
+
+    }
+
+    public void getGrade(){
+        System.out.println("----------Grade Report---------");
+        System.out.println("Subject                 Grade");
+        int i = 0;
+        for (String subject: subject){
+            System.out.print(subject+"                  ");
+            double mark = marks[i];
+            if (mark>75){
+                System.out.println('A');
+            }
+            else if (mark>65){
+                System.out.println('B');
+            }
+            else if (mark>55) {
+                System.out.println('C');
+            }
+            else {
+                System.out.println('W');
+            }
+            i++;
+        }
     }
 
 
--- src/oop/studentMarksProcessingSystem/Main.java
+++ src/oop/studentMarksProcessingSystem/Main.java
@@ -1,4 +1,17 @@
 package oop.studentMarksProcessingSystem;
 
 public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("90000","His Name");
+        System.out.println("Student Name : "+student.getName());
+        System.out.println("Student ID   : "+student.getId());
+
+        double [] marks = {80, 70, 60, 90, 40};
+        String [] subjects = {"English","Science","Sinhala","Maths","History"};
+
+        ExamResult examResult = new ExamResult(student,marks,subjects);
+        System.out.println("Total : "+examResult.getTotal());
+        System.out.println("Average : "+ examResult.getAverage());
+        examResult.getGrade();
+    }
 }
